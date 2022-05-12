3c46cc1ee524e91e1ece7948dace104f9c437e6a
AbdelRahim A. Elmadany <elmadany@gmail.com>
2022-05-12 12:20:31 -0700

    release 1.0

---
+++ build/lib/turjuman/metrics.py
@@ -0,0 +1,16 @@
+import sacrebleu
+
+
+
+def bleu_score(hyp_file, ref_file):
+    decoded_preds = open(hyp_file,'r').read().splitlines()
+    decoded_labels = open(ref_file,'r').read().splitlines()
+    if len(decoded_preds) != len(decoded_labels):
+        print ("[Error] The number of hypothesis (predicted) and references (labels) samples should be equaled " )
+    else:
+        bleu_results = sacrebleu.corpus_bleu(decoded_preds, [decoded_labels])
+        bleu_score = bleu_results.score
+        result = {"bleu": bleu_score}
+        return result
+
+# print (bleu_score("predict_sample.txt", "gold_sample.txt"))
