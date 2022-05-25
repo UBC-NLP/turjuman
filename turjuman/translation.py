7249e7ccb4ce0a137b4f70d089f319be1ad08bd9
AbdelRahim A. Elmadany <elmadany@gmail.com>
2022-05-24 17:42:28 -0700

    v1.0.2

--- turjuman/translation.py
+++ turjuman/translation.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from accelerate import Accelerator
-from transformers import default_data_collator
+from transformers import default_data_collator, Seq2SeqTrainer, Seq2SeqTrainingArguments, DataCollatorForSeq2Seq
 from datasets import load_dataset
 # import psutil
 # import math
@@ -15,8 +15,10 @@ class translate_from_file():
         self.cache_dir=cache_dir
         # self.num_cpus=len(psutil.Process().cpu_affinity())
         self.data_collator = default_data_collator
+        # self.data_collator  = DataCollatorForSeq2Seq(tokenizer, model=model)
         self.accelerator = Accelerator()
         self.gen_kwargs=None
+
 
 
     
@@ -38,12 +40,81 @@ class translate_from_file():
             )
         # self.logger.info("Tokenization process is done")
         return processed_datasets["source"]
+    # def translate_tranier(self, filepath, batch_size, gen_kwargs):
+    #     # self.model.resize_token_embeddings(len(self.tokenizer))
+    #     args = Seq2SeqTrainingArguments(
+    #             output_dir="./",
+    #             per_device_train_batch_size=batch_size,
+    #             per_device_eval_batch_size=batch_size,
+    #             predict_with_generate=True,
+    #             # fp16=True,
+    #             )
+
+    #     sources=self.get_file_data(filepath)
+    #     sources_dataloader = DataLoader(sources, collate_fn=self.data_collator, batch_size=batch_size)
+    #     # raw_datasets = load_dataset('text', data_files={'source': filepath}, cache_dir=self.cache_dir)
+    #     # predict_dataset = raw_datasets["source"]
+    #     # with args.main_process_first(desc="prediction dataset map pre-processing"):
+    #     #         predict_dataset = predict_dataset.map(
+    #     #         self.preprocess_function,
+    #     #         batched=True,
+    #     #         desc="Running tokenizer on prediction dataset",
+    #     #     )
+        
+
+    #     self.logger.warning(
+    #     f"Process rank: {args.local_rank}, device: {args.device}, n_gpu: {args.n_gpu}"
+    #     + f"distributed training: {bool(args.local_rank != -1)}, 16-bits training: {args.fp16}")
+    #     trainer = Seq2SeqTrainer(
+    #                 self.model, 
+    #                 args,
+    #                 # data_collator=self.data_collator,
+    #                 # tokenizer=self.tokenizer
+    #                 )
+
+    #     self.model.eval()
+    #     generated_text=[]
+    #     num_batches = len(sources_dataloader)
+    #     pbar = tqdm(total=num_batches, desc="translate")
+    #     self.logger.info("Translating with batch_size {} and #batches = {}".format(batch_size, num_batches))
+    #     for step, batch in enumerate(sources_dataloader):
+    #         with torch.no_grad():
+    #             outputs = self.model.generate(
+    #                     input_ids=batch["input_ids"], 
+    #                     attention_mask=batch["attention_mask"],
+    #                     **gen_kwargs,
+    #                 )
+
+    #             decoded_preds = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    #             generated_text.extend(decoded_preds)
+    #             pbar.update(1)
+    #     print (generated_text)
+    #     # self.model.to(args.device)
+    #     # predict_results = trainer.predict(
+    #     #     predict_dataset, metric_key_prefix="predict", max_length=300, num_beams=5
+    #     # )
+    #     # predictions = self.tokenizer.batch_decode(
+    #     #             predict_results.predictions, skip_special_tokens=True, clean_up_tokenization_spaces=True
+    #     #         )
+    #     # predictions = [pred.strip() for pred in predictions]
+    #     # print (predictions)
+    
     def translate(self, filepath, batch_size, gen_kwargs):
+        args = Seq2SeqTrainingArguments(
+                output_dir="./",
+                per_device_train_batch_size=batch_size,
+                per_device_eval_batch_size=batch_size,
+                predict_with_generate=True,
+                # fp16=True,
+                )
         self.gen_kwargs = gen_kwargs
+        # self.translate_tranier(filepath, batch_size, gen_kwargs)
         # print (gen_kwargs)
         sources=self.get_file_data(filepath)
         generated_text=[]
         sources_dataloader = DataLoader(sources, collate_fn=self.data_collator, batch_size=batch_size)
+        self.logger.info("Working on {}".format(args.device))
+        self.model.to(args.device)
         self.model.eval()
         samples_seen = 0
         num_batches = len(sources_dataloader)
@@ -62,7 +133,7 @@ class translate_from_file():
                 # decoded_preds = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
                 # generated_text.extend(decoded_preds)
                 # pbar.update(1)
-
+                #------------------------------ best -------------------------------------
                 generated_tokens = self.accelerator.unwrap_model(self.model).generate(
                     batch["input_ids"],
                     attention_mask=batch["attention_mask"],
@@ -81,6 +152,7 @@ class translate_from_file():
                 # print(decoded_preds)
                 generated_text.extend(decoded_preds)
                 pbar.update(1)
+                #------------------------------------------------------------------------------
         pbar.close()
         sources_text = [src['text'] for src in sources]
         outputs = extract_output(sources_text, generated_text, gen_kwargs['num_return_sequences'], self.logger)
